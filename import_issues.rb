c10c1cbd20de6f7a6a4634ecd85c40e348e487ba
Robb Shecter <robb@weblaws.org>
2013-10-27 18:32:17 -0700

    Handle Icebox and accepted stories by adding GitHub labels beginning with PT-. Also, quieted down the debug output.

--- import_issues.rb
+++ import_issues.rb
@@ -10,6 +10,18 @@ require 'highline/import'
 def get_input(prompt="Enter >",show = true)
      ask(prompt) {|q| q.echo = show}
 end
+
+
+def add_pt_label(labels, text)
+  result = labels
+  if result != ''
+    result += ','
+  end
+  result += text
+  return result
+end
+
+
 filename = ARGV.shift or raise "Enter Filepath to CSV as ARG1"
 
 class GitHub
@@ -33,13 +45,31 @@ CSV.open filename, :headers => true do |csv|
     }
     labels = []
 
-    # Give bugs the "bug" label.
+
+    # Give PT Bugs the GitHub "bug" label.
     if r['Story Type'] == 'bug'
-      if r['Labels'] != ''
-        r['Labels'] += ','
-      end
-      r['Labels'] += 'bug'
+      r['Labels'] = add_pt_label(r['Labels'], 'bug')
     end
+
+    # Give PT Icebox stories the "PT-icebox" GitHub label.
+    if r['Current State'] == 'unscheduled'
+      r['Labels'] = add_pt_label(r['Labels'], 'PT-icebox')
+    end
+
+    # Give completed PT stories the "PT-accepted" GitHub label.
+    # GitHub's Issues API doesn't allow the creation of closed
+    # issues:
+    # http://developer.github.com/v3/issues/#create-an-issue
+    # So this label enables the user to convert the accepted
+    # items to the GitHub closed status.
+    # TODO: Use the Issue/edit API to change status to closed.
+    #
+    # If this isn't desired, then simply don't export done
+    # stories from Pivotal Tracker.
+    if r['Current State'] == 'accepted'
+      r['Labels'] = add_pt_label(r['Labels'], 'PT-accepted')
+    end
+
 
     if r['Labels'] != ''
       r['Labels'].split(',').each do |label|
@@ -53,21 +83,22 @@ CSV.open filename, :headers => true do |csv|
        end
       end
       labels.each do |label|
-        p label
+        # p label
         # this hack doesn't care if you have an existing label - it just errors and moves on like a zen master
         # the server however is expected to be equally zen :D
         visited_labels << label[:name]
         label = GitHub.post "/repos/#{repository_path}/labels", :body => JSON.generate(label)
-        p label
+        # p label
       end
     end
     
     body[:labels] = r['Labels'].split(',').map {|l|l.strip} if r['Labels'] != ''
 
    
-    p json_body = JSON.generate(body)
+    p body[:title]
+    json_body = JSON.generate(body)
     issue = GitHub.post "/repos/#{repository_path}/issues", :body => json_body
-    p issue
+    # p issue
 
     r.each do |f|
       if f[0] == 'Note'
